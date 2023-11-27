27f7d064b5531205633fc16548bbf3b6a77589e3
Edward Wang <edward.yu.wang@gmail.com>
2023-11-27 09:11:31 +0800

    Introduce r3f applyProps
    The performace could suffer comparing to directly manipulate the
    properties of underlying component though.

--- src/React/Basic/R3F/Hooks.js
+++ src/React/Basic/R3F/Hooks.js
@@ -1,4 +1,8 @@
-import { useFrame } from "@react-three/fiber"
+import { useFrame, applyProps } from "@react-three/fiber"
 
 export const useFrameImpl = useFrame
+export const applyRefPropsImpl = (ref, props) => {
+  applyProps(ref.current, props)
+  return
+}
 
--- src/React/Basic/R3F/Hooks.purs
+++ src/React/Basic/R3F/Hooks.purs
@@ -3,14 +3,17 @@ module React.Basic.R3F.Hooks where
 import Prelude
 
 import Effect (Effect)
-import Effect.Uncurried (EffectFn1, EffectFn2, mkEffectFn2, runEffectFn1)
+import Effect.Uncurried (EffectFn1, EffectFn2, mkEffectFn2, runEffectFn1, runEffectFn2)
+import React.Basic (JSX, Ref)
 import React.Basic.Hooks (Hook, unsafeHook)
 import React.Basic.R3F.Types as Three
 
 foreign import data UseFrame :: Type -> Type -> Type
 
 foreign import useFrameImpl
-  :: EffectFn1 (EffectFn2 { | RootState } Number Unit) Unit
+  :: EffectFn1 (EffectFn2 (Record RootState) Number Unit) Unit
+foreign import applyRefPropsImpl
+  :: forall props. EffectFn2 (Ref JSX) (Record props) Unit
 
 type RootState =
   ( clock :: Three.Clock
@@ -18,7 +21,14 @@ type RootState =
   )
 
 useFrame
-  :: ({ | RootState } -> Number -> Effect Unit)
-  -> Hook (UseFrame { | RootState }) Unit
+  :: ((Record RootState) -> Number -> Effect Unit)
+  -> Hook (UseFrame (Record RootState)) Unit
 useFrame = unsafeHook <<< runEffectFn1 useFrameImpl <<< mkEffectFn2
+
+applyRefProps
+  :: forall props
+   . Ref JSX
+  -> Record props
+  -> Effect Unit
+applyRefProps = runEffectFn2 applyRefPropsImpl
 
