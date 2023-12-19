322933b9f41895f139f5c84e9a43181d35764e33
Edward Wang <edward.yu.wang@gmail.com>
2023-12-19 20:41:28 +0800

    Introduce the `useTextures` function

--- src/React/R3F/Drei/Loaders.js
+++ src/React/R3F/Drei/Loaders.js
@@ -1,6 +1,7 @@
 import { useTexture, useGLTF, useEnvironment } from "@react-three/drei"
 
 export const useTextureImpl = useTexture
+export const useTexturesImpl = useTexture
 export const useGLTFImpl = useGLTF
 export const preloadGLTFImpl = useGLTF.preload
 export const useEnvImpl = useEnvironment
--- src/React/R3F/Drei/Loaders.purs
+++ src/React/R3F/Drei/Loaders.purs
@@ -3,11 +3,50 @@ module React.R3F.Drei.Loaders where
 import Prelude
 
 import Effect.Uncurried (EffectFn1, runEffectFn1)
+import Prim.RowList (class RowToList)
 import React.Basic.Hooks (Hook, unsafeHook)
 import React.R3F.Three.Types (Texture)
+import Record.Extra (class MapRecord)
 
 useTexture :: forall hooks. String -> Hook (UseLoader hooks) Texture
 useTexture = unsafeHook <<< runEffectFn1 useTextureImpl
+
+-- | Load multiple textures at once.
+-- |
+-- | The type signature of the function dictates that given an uniform record of
+-- | all string arguments, it will return a record of the same shape but with
+-- | all texture results.
+-- |
+-- | E.g., in physically based rendering (PBR) maps, there are several maps that
+-- | serve different purposes, such as color map, normal map, displacement map,
+-- | etc. To load them:
+-- | ```purescript
+-- |  textures <- useTextures
+-- |    { colorMap: "path/to/color_map_file"
+-- |    , normalMap: "..."
+-- |    , displacementMap: "..."
+-- |    , aoMap: "..."
+-- |    , roughnessMap: "..."
+-- |    , metalicMap: "..."
+-- |    }
+-- | ```
+-- | Then the result `textures` will be a record of the type:
+-- | ```purescript
+-- |    { colorMap :: Texture
+-- |    , normalMap :: Texture
+-- |    , displacementMap :: Texture
+-- |    , aoMap :: Texture
+-- |    , roughnessMap :: Texture
+-- |    , metalicMap :: Texture
+-- |    }
+-- | ```
+useTextures
+  :: forall rl ins outs hooks
+   . RowToList ins rl
+  => MapRecord rl ins String Texture () outs
+  => { | ins }
+  -> Hook (UseLoader hooks) { | outs }
+useTextures = unsafeHook <<< runEffectFn1 useTexturesImpl
 
 -- | A GLTF loader hook.
 -- |
@@ -56,6 +95,7 @@ useEnvironment file = unsafeHook $ runEffectFn1 useEnvImpl { files: file }
 foreign import data UseLoader :: Type -> Type -> Type
 
 foreign import useTextureImpl :: EffectFn1 String Texture
+foreign import useTexturesImpl :: forall a b. EffectFn1 { | a } { | b }
 foreign import useGLTFImpl :: forall a. EffectFn1 String a
 foreign import preloadGLTFImpl :: EffectFn1 String Unit
 foreign import useEnvImpl :: forall props. EffectFn1 { | props } Texture
